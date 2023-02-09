fd41826bac2cebc491f849222b4576189c71cacf
softsektor! <bymbanym22@gmail.com>
2023-02-09 19:46:26 +0800

    git commit -m "shoog shiddeg bolgow "

--- app.js
+++ app.js
@@ -7,12 +7,18 @@ var score = [0, 0];
 // тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадаглах хувьсагч хэргэтэй
 var roundScore = 0;
 // шааний аль талаараа буусныг хадаглах хувисагч хэрэгтэй 1-6 гэсэн утгыг энэ хувсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random() * 6) + 1;
-// document.querySelector("#score-0").textContent = dice;
-// document.querySelector("#score-1").textContent = dice;
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
-console.log(dice);
+
+// programm ehelhed beldey
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "inline-block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
