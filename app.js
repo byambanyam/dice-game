68e6a77f23b7c92282fc8fc10b3bad70acd9840b
softsektor! <bymbanym22@gmail.com>
2023-02-09 21:27:43 +0800

    activiig tohiruullaa

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // тоглогчийн ээлжийг хадгалах хувисагч хэрэгтэй
-var activePlayer = 1;
+var activePlayer = 0;
 
 // тоглогчдын цуглуулсан оноог хадаглах хувьсагч хэрэгтэй
 var score = [0, 0];
@@ -16,9 +16,31 @@ document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
-
+// shoo shideh heseg
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1-6 Хүртэл  санамсаргүй нэг то гаргаж авна
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+  //   шооны зэргыг вэб дээр гаргаж ирнэ
   diceDom.style.display = "inline-block";
+  //   олсон санамсаргүй утгад харгалзах шооны зургыг вэб дээр үзүүлэнэ.
+
   diceDom.src = "dice-" + diceNumber + ".png";
+  //  buusan toon 1 ees ylgaatai bol idwehtei tolgogchiin eeljiin toog nemegduulne toglogchiin eeljiin onoog oorchilno oo
+  if (diceNumber !== 1) {
+    // 1-s ylgaatai too buulaa
+
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    // ene toglogchiin eejindee tsugluulsan onoog 0 bolgono
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    // ene eelj shiljuulene
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    // activ player tohiruulah buyu ulaan tsaeg
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    // shoog tu alga bologonoo
+    diceDo.style.display = "none";
+  }
 });
